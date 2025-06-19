cc5a23e56f3b2144af304d4a84a227bb8b037273
Stacy Kamau <kamaustacywaithera@gmail.com>
2025-06-19 23:31:23 +0300

    Minor changes to body

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
           <div className="home-content">
             <div className="home-text">
               <h2>Hi, I'm Stacy Kamau</h2>
-              <h3>Front-End Web Developer & Junior Software Engineer</h3>
+              <h3>Front-End Web Developer & Junior Software Developer</h3>
             </div>
 
             <div className="home-image-container">
